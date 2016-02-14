9a758c17f28edac09783805b9e1af4c635242c17
Keith Cirkel <github@keithcirkel.co.uk>
2016-02-14 15:08:32 +0000

    fix: allow provision-npm-babel to be run without config

--- src/index.js
+++ src/index.js
@@ -4,33 +4,40 @@ import jsonFile from 'packagesmith.formats.json';
 import { runProvisionerSet } from 'packagesmith';
 import sortPackageJson from 'sort-package-json';
 const babelVersionFive = 5;
-export function provisionNpmBabel(config) {
+export function provisionNpmBabel({
+  babelConfig,
+  scriptName = 'prepublish',
+  babelVersion,
+  babelPresets = null,
+} = {}) {
   return {
     'package.json': {
       after: 'npm install',
       contents: jsonFile((contents) => {
         const packageJson = {
-          babel: config.babelConfig || {
+          babel: babelConfig || {
             compact: false,
             ignore: 'node_modules',
             sourceMaps: 'inline',
           },
           devDependencies: {},
           scripts: {
-            [config.scriptName || 'prepublish']: 'babel $npm_package_directories_src -d $npm_package_directories_lib',
+            [scriptName]: 'babel $npm_package_directories_src -d $npm_package_directories_lib',
           },
         };
-        if (config.babelVersion === babelVersionFive) {
+        if (babelVersion === babelVersionFive) {
           packageJson.devDependencies.babel = '^5.8.34';
         } else {
+          if (!babelPresets) {
+            babelPresets = { 'es2015': '^6.5.0' };
+          }
           packageJson.devDependencies['babel-cli'] = '^6.5.1';
           packageJson.devDependencies['babel-core'] = '^6.5.2';
           packageJson.babel.presets = [];
-          const presets = config.babelPresets || { 'es2015': '^6.5.0' };
-          Object.keys(presets).forEach((preset) => {
+          Object.keys(babelPresets).forEach((preset) => {
             const shorthand = preset.replace(/^babel-preset-/, '');
             const longhand = `babel-preset-${ shorthand }`;
-            packageJson.devDependencies[longhand] = presets[preset];
+            packageJson.devDependencies[longhand] = babelPresets[preset];
             packageJson.babel.presets.push(shorthand);
           });
         }
