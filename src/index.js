c8d28d2ae3f9889377a02bdf9ce06c64a0427a21
Keith Cirkel <keithamus@users.noreply.github.com>
2016-04-20 00:07:53 +0100

    refactor: move code configuring babel 6 into own function

--- src/index.js
+++ src/index.js
@@ -17,24 +17,13 @@ function configureBabelFive(packageJson, babelStage, babelRuntime) {
     packageJson.dependencies['babel-runtime'] = versions.five['babel-runtime'];
   }
 }
+
 function defaultBabelPresets(stage) {
   const presets = { 'es2015': versions.six['babel-preset-es2015'] };
   if (typeof stage === 'number') {
     presets[`stage-${ stage }`] = versions.six[`babel-preset-stage-${ stage }`];
   }
   return presets;
-}
-
-function determineExistingBabelConfig(existingPackageJson) {
-  const config = {};
-  if ('babel' in (existingPackageJson.devDependencies || {})) {
-    delete existingPackageJson.devDependencies.babel;
-    if ('stage' in (existingPackageJson.babel || {})) {
-      config.babelStage = (existingPackageJson.babel || {}).stage;
-      delete existingPackageJson.babel.stage;
-    }
-  }
-  return config;
 }
 
 function configurePlugins(packageJson, babelPlugins) {
@@ -47,6 +36,39 @@ function configurePlugins(packageJson, babelPlugins) {
   if (plugins.length) {
     packageJson.babel.plugins = plugins;
   }
+}
+
+function configureBabelSix(packageJson, babelStage, babelPresets, babelPlugins, babelRuntime) {
+  // Detect and remove old babel 5 config
+  if ('babel' in packageJson.devDependencies) {
+    delete packageJson.devDependencies.babel;
+    if ('stage' in packageJson.babel) {
+      babelStage = packageJson.babel.stage;
+      delete packageJson.babel.stage;
+    }
+  }
+  if (!babelPresets) {
+    babelPresets = defaultBabelPresets(babelStage);
+  }
+  if (babelRuntime) {
+    packageJson.dependencies = packageJson.dependencies || {};
+    if (!packageJson.dependencies['babel-runtime']) {
+      packageJson.dependencies['babel-runtime'] = versions.six['babel-runtime'];
+    }
+    if (!babelPlugins['transform-runtime']) {
+      babelPlugins['transform-runtime'] = versions.six['babel-plugin-transform-runtime'];
+    }
+  }
+  packageJson.devDependencies['babel-cli'] = versions.six['babel-cli'];
+  packageJson.devDependencies['babel-core'] = versions.six['babel-core'];
+  packageJson.babel.presets = [];
+  Object.keys(babelPresets).forEach((preset) => {
+    const shorthand = preset.replace(/^babel-preset-/, '');
+    const longhand = `babel-preset-${ shorthand }`;
+    packageJson.devDependencies[longhand] = babelPresets[preset];
+    packageJson.babel.presets.push(shorthand);
+  });
+  configurePlugins(packageJson, babelPlugins);
 }
 
 export function provisionNpmBabel({
@@ -62,7 +84,7 @@ export function provisionNpmBabel({
     'package.json': {
       after: 'npm install',
       contents: jsonFile((contents) => {
-        const packageJson = {
+        const packageJson = defaultsDeep({
           babel: babelConfig || {
             compact: false,
             ignore: 'node_modules',
@@ -72,42 +94,18 @@ export function provisionNpmBabel({
           scripts: {
             [scriptName]: 'babel $npm_package_directories_src -d $npm_package_directories_lib',
           },
-        };
-        if (babelVersion === babelVersionFive) {
-          configureBabelFive(packageJson, babelStage, babelRuntime);
-        } else {
-          // Detect and remove old babel 5 config
-          const existingConfig = determineExistingBabelConfig(contents);
-          babelStage = existingConfig.babelStage || babelStage;
-          if (!babelPresets) {
-            babelPresets = defaultBabelPresets(babelStage);
-          }
-          if (babelRuntime) {
-            packageJson.dependencies = packageJson.dependencies || {};
-            if (!packageJson.dependencies['babel-runtime']) {
-              packageJson.dependencies['babel-runtime'] = versions.six['babel-runtime'];
-            }
-            if (!babelPlugins['transform-runtime']) {
-              babelPlugins['transform-runtime'] = versions.six['babel-plugin-transform-runtime'];
-            }
-          }
-          packageJson.devDependencies['babel-cli'] = versions.six['babel-cli'];
-          packageJson.devDependencies['babel-core'] = versions.six['babel-core'];
-          packageJson.babel.presets = [];
-          Object.keys(babelPresets).forEach((preset) => {
-            const shorthand = preset.replace(/^babel-preset-/, '');
-            const longhand = `babel-preset-${ shorthand }`;
-            packageJson.devDependencies[longhand] = babelPresets[preset];
-            packageJson.babel.presets.push(shorthand);
-          });
-          configurePlugins(packageJson, babelPlugins);
-        }
-        return sortPackageJson(defaultsDeep(packageJson, contents, {
+        }, contents, {
           directories: {
             lib: 'lib',
             src: 'src',
           },
-        }));
+        });
+        if (babelVersion === babelVersionFive) {
+          configureBabelFive(packageJson, babelStage, babelRuntime);
+        } else {
+          configureBabelSix(packageJson, babelStage, babelPresets, babelPlugins, babelRuntime);
+        }
+        return sortPackageJson(packageJson);
       }),
     },
   };
